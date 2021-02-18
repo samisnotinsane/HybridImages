01b9a030aac04d6c4c24725b9874132e39ab3bbf
samisnotinsane <sameen11@hotmail.com>
2021-02-18 16:20:46 +0000

    Adding scaffolds.

---
+++ MyConvolution.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def convolve(image: np.ndarray, kernel: np.ndarray) -> np.ndarray:
+    """
+    Convolve an image with a kernel assuming zero-padding of the image to handle the borders
+
+    :param image: the image (either greyscale shape=(rows, cols) or colour shape=(rows, cols, channels))
+    :type numpy.ndarray
+
+    :param: kernel: the kernel (shape=(kheight, kwidth); both dimensions odd)
+    :type: numpy.ndarray
+
+    :returns the convolved image (of the same shape as the input image)
+    :rtype numpy.ndarray
+    """
+    return np.zeros((1,1))
